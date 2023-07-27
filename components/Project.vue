57ac3f7705b6316535b1a1f4b9ddb8f3a9935b15
Yongpan Sheng <shengyp2011@yeah.net>
2023-07-27 15:14:44 +0800

    Add files via upload

---
+++ components/Project.vue
@@ -0,0 +1,122 @@
+<script setup>
+
+import { computed, ref } from "vue";
+import projectImg from '../assets/project_img.png'
+
+// 精选项目
+const projects = [
+    {
+        title: "新闻知识图谱",
+        image: projectImg,
+        description:
+            "地理时空知识图谱是一种基于地理信息和时间信息的知识表示方式，它将地理空间信息和时间信息结合起来，形成一个具有层次结构的知识图谱。",
+        paper:
+            "https://link.springer.com/content/pdf/10.1007/978-3-030-10997-4_54.pdf",
+        system: "https://shengyp.github.io/vmse/",
+    },
+    {
+        title: "地理知识图谱",
+        image: projectImg,
+        description: "可自行安排形成一个具有层次结构的知识图谱。",
+        paper:
+            "https://link.springer.com/content/pdf/10.1007/978-3-030-10997-4_54.pdf",
+        system: "https://shengyp.github.io/vmse/",
+    },
+    {
+        title: "zzaaaaa",
+        image: projectImg,
+        description: "可自行安排形成一个具有层次结构的知识图谱。",
+        paper:
+            "https://link.springer.com/content/pdf/10.1007/978-3-030-10997-4_54.pdf",
+        system: "https://shengyp.github.io/vmse/",
+    },
+    {
+        title: "BBBBaa",
+        image: projectImg,
+        description: "可自行安排形成一个具有层次结构的知识图谱。",
+        paper:
+            "https://link.springer.com/content/pdf/10.1007/978-3-030-10997-4_54.pdf",
+        system: "https://shengyp.github.io/vmse/",
+    }
+];
+</script>
+<template>
+    <br>
+
+    <!-- <p style="margin-top: -5px"> -->
+    <!-- </p> -->
+    <hr />
+
+    <div>
+        <div style="display: flex;justify-content:space-between;margin-top: 15px;align-items: center;">
+            <p>
+                <font size="5" face="Times New Roman"><strong><a name="pub">精选项目</a></strong></font>
+            </p>
+            <div style="margin-right: 20px;">
+                <a href="/project">
+                    <button class="more-button">
+                        查看更多
+                    </button>
+                </a>
+            </div>
+        </div>
+
+        <div>
+            <div
+                style="display: grid;grid-template-columns: repeat(3,275px);gap: 10px;justify-content: center;justify-items: center;">
+                <div class="box-1" v-for="(items, index) in projects" :key="index">
+                    <div style="font-weight: bold; height: 30px; line-height: 30px">
+                        {{ items.title }}
+                    </div>
+                    <div>
+                        <img :src="items.image" style="width: 240px" alt="" srcset="" />
+                    </div>
+                    <div style="font-size: 0.85em; margin: 0.7em 0; line-height: 1.2">
+                        {{ items.description }}
+                    </div>
+                    <div class="links">
+                        <a :href="items.paper">
+                            <el-icon>
+                                <Tickets style="width: 1em; height: 1em" />
+                            </el-icon>
+                            论文
+                        </a>
+                        <a :href="items.system" target="_blank">
+                            <el-icon>
+                                <Monitor style="width: 1em; height: 1em" />
+                            </el-icon>
+                            工程</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+
+<style scoped>
+.box-1 {
+    border: 1px solid #eee;
+    height: 260px;
+    width: 240px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    align-items: flex-start;
+    padding: 16px;
+    transition: box-shadow 0.2s ease;
+}
+
+.box-1:hover {
+    box-shadow: 1px 1px 10px 1px rgb(171, 171, 171);
+}
+
+.links {
+    -webkit-text-size-adjust: 100%;
+    font-family: "Fira Sans", sans-serif;
+    line-height: 1.8;
+    font-size: 0.8em;
+    display: flex;
+    gap: 10px;
+}
+</style>
---
+++ components/Service.vue
@@ -0,0 +1,257 @@
+<script setup></script>
+<template>
+    <div>
+        <br>
+
+        <!-- <p id="service" style="margin-top: 5px"></p> -->
+
+        <hr />
+
+        <p>
+            <font size="5" face="Times New Roman"><strong><a name="pub">学术服务</a></strong>
+            </font>
+        </p>
+        <span style="font-weight: bold"> 担任以下学术期刊客座编辑: </span>
+        <ul>
+            <li class="academic_service">
+                Entropy (JCR Q2, IF: 2.738), 2022-2023
+            </li>
+        </ul>
+        <span style="font-weight: bold">
+            担任以下学术会议宣传主席 (Publicity Chair):
+        </span>
+        <ul>
+            <li class="academic_service">
+                China Conference on Knowledge Graph and Semantic Computing (CCKS
+                2023)
+            </li>
+        </ul>
+        <span style="font-weight: bold">
+            担任以下学术会议本地主席 (Local Chair):
+        </span>
+        <ul>
+            <li class="academic_service">
+                China Conference on Knowledge Graph and Semantic Computing (CCKS
+                2024)
+            </li>
+        </ul>
+        <span style="font-weight: bold">
+            担任以下学术会议专题分组共同主席 (Co-Chair)
+        </span>
+        <ul>
+            <li class="academic_service">
+                The 2023 International Conference on Cyber-physical Social
+                Intelligence (ICCSI 2023, Special Session: Big Data Techniques
+                for Computer Vision
+            </li>
+        </ul>
+        <span style="font-weight: bold">
+            牵头在以下学术期刊上组织主题专刊:
+        </span>
+        <ul>
+            <li class="academic_service">
+                Remote Sensing (SCI, JCR Q1), Special Issue: "Knowledge
+                Computation in Muti-Source, Multi-Granularity, Multi-Modal
+                Remote Sensing Data"
+            </li>
+            <li class="academic_service">
+                Mathematics (SCI, JCR Q1), Special Issue: "When Mathematical
+                Learning Meets Knowledge Computation: In Pursuit of Advanced
+                Computational Theories, Paradigms, and Methods"
+            </li>
+            <li class="academic_service">
+                Applied Sciences-Basel (SCI, JCR Q2), Special Issue: "New
+                Advances for Open-Domain Information Mining in Theories and
+                Applications"
+            </li>
+            <li class="academic_service">
+                Entropy (SCI, JCR Q2), Special Issue: "Information Network
+                Mining and Applications"
+            </li>
+        </ul>
+        <span style="font-weight: bold">
+            担任以下学术会议程序委员会成员:
+        </span>
+        <ul>
+            <li class="academic_service">
+                China Conference on Knowledge Graph and Semantic Computing (CCKS
+                2023)
+            </li>
+            <li class="academic_service">
+                The 22nd China National Conference on Computational Linguistics
+                (CCL 2023)
+            </li>
+            <li class="academic_service">
+                The 7th APWeb-WAIM International Joint Conference on Web and Big
+                Data (APWeb-WAIM 2023)
+            </li>
+            <li class="academic_service">
+                The 28th International Conference on Database Systems for
+                Advanced Applications (DASFAA 2023)
+            </li>
+            <li class="academic_service">
+                The 21st China National Conference on Computational Linguistics
+                (CCL 2022)
+            </li>
+            <li class="academic_service">
+                The 8th International Conference on Data Mining (DaMi 2022)
+            </li>
+            <li class="academic_service">
+                The 29th International Conference on Neural Information
+                Processing (ICONIP 2022)
+            </li>
+            <li class="academic_service">
+                China Conference on Knowledge Graph and Semantic Computing (CCKS
+                2022)
+            </li>
+            <li class="academic_service">
+                International Conference on Images, Signals, and Computing
+                (ICISC 2023)
+            </li>
+            <li class="academic_service">
+                International Conference Advanced Data Mining and Applications
+                (ADMA 2022)
+            </li>
+            <li class="academic_service">
+                The European Conference on Machine Learning and Principles and
+                Practice of Knowledge Discovery in Databases (ECML-PKDD 2022)
+            </li>
+            <li class="academic_service">
+                The 6th APWeb-WAIM Joint Conference on Web and Big Data
+                (APWeb-WAIM 2022)
+            </li>
+            <li class="academic_service">
+                The 5th APWeb-WAIM Joint Conference on Web and Big Data
+                (APWeb-WAIM 2021)
+            </li>
+            <li class="academic_service">
+                The 4th APWeb-WAIM Joint Conference on Web and Big Data
+                (APWeb-WAIM 2020)
+            </li>
+        </ul>
+        <span style="font-weight: bold"> 担任以下学术期刊审稿人: </span>
+        <ul>
+            <li class="academic_service">
+                CAAI Transactions on Intelligence Technology (IF: 7.985 in 2021)
+            </li>
+            <li class="academic_service">
+                Neurocomputing (Neurocomputing, IF: 5.779 in 2021)
+            </li>
+            <li class="academic_service">
+                Knowledge-Based Systems (KBS, IF: 5.101 in 2018)
+            </li>
+            <li class="academic_service">
+                Pattern Recognition Letters (PR Letters, IF: 2.810 in 2018)
+            </li>
+            <li class="academic_service">IEEE Access (IF: 3.557 in 2017)</li>
+            <li class="academic_service">
+                Applied Sciences (AS, IF: 2.838 in 2021)
+            </li>
+            <li class="academic_service">Entropy (IF: 2.738 in 2021)</li>
+            <li class="academic_service">
+                Sustainability (IF: 3.889 in 2021)
+            </li>
+            <li class="academic_service">Data and Information Management</li>
+            <li class="academic_service">Data Intelligence</li>
+            <li class="academic_service">
+                Computational Intelligence and Neuroscience
+            </li>
+            <li class="academic_service">Frontiers of Computer Science</li>
+            <li class="academic_service">Sensors</li>
+        </ul>
+        <span style="font-weight: bold"> 担任以下学术会议审稿人: </span>
+        <ul>
+            <li class="academic_service">
+                The 30th International Conference on Neural Information
+                Processing (ICONIP 2023)
+            </li>
+            <li class="academic_service">
+                China Conference on Knowledge Graph and Semantic Computing (CCKS
+                2023)
+            </li>
+            <!--<li>The 22nd China National Conference on Computational Linguistics
+                            (CCL 2023)
+                        </li>-->
+            <li class="academic_service">
+                The 7th APWeb-WAIM International Joint Conference on Web and Big
+                Data (APWeb-WAIM 2023)
+            </li>
+            <li class="academic_service">
+                The 28th International Conference on Database Systems for
+                Advanced Applications (DASFAA 2023)
+            </li>
+            <li class="academic_service">
+                The 21st China National Conference on Computational Linguistics
+                (CCL 2022)
+            </li>
+            <li class="academic_service">
+                The 8th International Conference on Data Mining (DaMi 2022)
+            </li>
+            <li class="academic_service">
+                The 29th International Conference on Neural Information
+                Processing (ICONIP 2022)
+            </li>
+            <li class="academic_service">
+                China Conference on Knowledge Graph and Semantic Computing (CCKS
+                2022)
+            </li>
+            <li class="academic_service">
+                International Conference on Images, Signals, and Computing
+                (ICISC 2023)
+            </li>
+            <li class="academic_service">
+                International Conference Advanced Data Mining and Applications
+                (ADMA 2022)
+            </li>
+            <li class="academic_service">
+                The European Conference on Machine Learning and Principles and
+                Practice of Knowledge Discovery in Databases (ECML-PKDD 2022)
+            </li>
+            <li class="academic_service">
+                The 6th APWeb-WAIM Joint Conference on Web and Big Data
+                (APWeb-WAIM 2022)
+            </li>
+            <li class="academic_service">
+                The 5th APWeb-WAIM Joint Conference on Web and Big Data
+                (APWeb-WAIM 2021)
+            </li>
+            <li class="academic_service">
+                The 4th APWeb-WAIM Joint Conference on Web and Big Data
+                (APWeb-WAIM 2020)
+            </li>
+            <li class="academic_service">
+                The 3rd APWeb-WAIM Joint Conference on Web and Big Data
+                (APWeb-WAIM 2019)
+            </li>
+            <li class="academic_service">
+                The International Workshop on Recommender Systems with Big Data
+                (RSBD 2019)
+            </li>
+            <li class="academic_service">
+                The 1st CCF International Conference on Artificial Intelligence
+                (CCF-ICAI 2018)
+            </li>
+            <li class="academic_service">
+                The 30th AAAI Conference on Artificial Intelligence (AAAI 2016)
+            </li>
+            <!--<li>2016全国知识图谱与语义计算大会 (CCKS 2016)</li>-->
+            <li class="academic_service">
+                The 7th Chinese Conference on Pattern Recognition (CCPR 2016)
+            </li>
+            <li class="academic_service">
+                The 2016 IEEE Symposium Series on Computational Intelligence
+                (IEEE SSCI 2016)
+            </li>
+            <!--<li>第四届CCF大数据学术会议 (CCF Big Data 2016)</li>-->
+        </ul>
+
+    </div>
+</template>
+
+<style scoped>
+.academic_service {
+    margin: 6px 0;
+    line-height: 18px;
+    font-size: medium;
+}
+</style>
