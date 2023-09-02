fe03806c268b5c5558ff9b924f54eba379546c83
Zshunxin <126639787+Zshunxin@users.noreply.github.com>
2023-09-02 18:54:04 +0800

    Update Thanks.vue

--- src/components/Thanks.vue
+++ src/components/Thanks.vue
@@ -19,11 +19,13 @@
     <font size="5" face="Times New Roman"><strong><a name="pub">致谢</a></strong></font>
   </p>
 
-<!--<font size="4" style="padding: 0px 0">Our research is supported generously by The Hong Kong Polytechnic
+  <!--<font size="4" style="padding: 0px 0">Our research is supported generously by The Hong Kong Polytechnic
     University, Hong Kong RGC, Tencent Technology Limited, etc.
   </font>-->
-  <div style="height: 2px;"></div>
-    <img src="../logo/UESTC_logo.png" style="height: 80px;margin: 6px;" alt="" srcset="">
-    <img src="../logo/unsw_logo.png" style="height: 100px;margin: 10px;" alt="" srcset="">
-    <img src="../logo/cqu_logo.jpeg" style="height: 100px;margin: 10px;" alt="" srcset="">
+  <!-- <div style="height: 2px;"></div> -->
+  <div style="display: flex;gap:10px;height: 100px;align-items: center;">
+    <img src="../logo/UESTC_logo.png" style="height: 60px;" alt="" srcset="">
+    <img src="../logo/unsw_logo.png" style="height: 100px;" alt="" srcset="">
+    <img src="../logo/cqu_logo.jpeg" style="height: 100px;" alt="" srcset="">
+  </div>
 </template>
