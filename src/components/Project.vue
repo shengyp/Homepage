fa8afee97c750ae77718862a66740508d46ef1c9
Zshunxin <126639787+Zshunxin@users.noreply.github.com>
2023-09-02 18:54:29 +0800

    Update Project.vue

--- src/components/Project.vue
+++ src/components/Project.vue
@@ -53,7 +53,7 @@ const projects = [
                 <font size="5" face="Times New Roman"><strong><a name="pub">精选项目</a></strong></font>
             </p>
             <div style="margin-right: 20px;">
-                
+
                 <router-link to="/project">
                     <button class="more-button">
                         查看更多
@@ -100,61 +100,68 @@ const projects = [
                 <p class="project-text">主持</p>
             </h3>
 
-            <div>
-                <span>省部级（含）以上科研项目</span>
+            <div class="project-item">
+                <span class="project-item-name">省部级（含）以上科研项目</span>
 
                 <ul class="">
                     <li>
-                        <div>国家自然科学基金委员会，国家自然科学基金青年科学基金项目：<span style="font-weight: bold">“面向非结构化文本数据的开放域知识图谱构建方法研究”</span>，62202075，2023.01-2025.12，在研。
+                        <div>国家自然科学基金委员会，国家自然科学基金青年科学基金项目：<span
+                                style="font-weight: bold">“面向非结构化文本数据的开放域知识图谱构建方法研究”</span>，62202075，2023.01-2025.12，在研。
                         </div>
                     </li>
-                    
+
                     <li>
-                        <div>重庆市科学技术局, 2022年度重庆市自然科学基金面上项目：<span style="font-weight: bold">“面向开放环境的知识图谱构建方法研究”</span>，2022NSCQ-MSX3749，2022.09-2025.09，在研。
+                        <div>重庆市科学技术局, 2022年度重庆市自然科学基金面上项目：<span
+                                style="font-weight: bold">“面向开放环境的知识图谱构建方法研究”</span>，2022NSCQ-MSX3749，2022.09-2025.09，在研。
                         </div>
                     </li>
                 </ul>
             </div>
 
 
-            <div>
-                <span>开放基金项目</span>
+            <div class="project-item">
+                <span class="project-item-name">开放基金项目</span>
 
-                <ul class="">                    
+                <ul class="">
                     <li>
-                        <div>2022年度海南师范大学数据科学与智慧教育教育部重点实验室开放课题：<span style="font-weight: bold">“基于非结构化文本数据的开放域知识图谱构建方法研究”</span>，2022NSCQ-MSX3749，2023.01-2024.12，在研。
+                        <div>2022年度海南师范大学数据科学与智慧教育教育部重点实验室开放课题：<span
+                                style="font-weight: bold">“基于非结构化文本数据的开放域知识图谱构建方法研究”</span>，2022NSCQ-MSX3749，2023.01-2024.12，在研。
                         </div>
                     </li>
 
                     <li>
-                        <div>农业农村部黄淮海智慧农业技术重点实验室2023年度开放课题：<span style="font-weight: bold">“基于农业科学数据的农作物碳效知识图谱构建与可视化方法研究”</span>，202307，2023.01-2024.12，在研。
+                        <div>农业农村部黄淮海智慧农业技术重点实验室2023年度开放课题：<span
+                                style="font-weight: bold">“基于农业科学数据的农作物碳效知识图谱构建与可视化方法研究”</span>，202307，2023.01-2024.12，在研。
                         </div>
                     </li>
 
                     <li>
-                        <div>安徽省北斗精准农业信息工程实验室2023年度开放基金项目：<span style="font-weight: bold">“基于文本挖掘的农作物基因表型知识图谱构建方法研究”</span>，BDSY2023004，2023.04-2025.04，在研。
+                        <div>安徽省北斗精准农业信息工程实验室2023年度开放基金项目：<span
+                                style="font-weight: bold">“基于文本挖掘的农作物基因表型知识图谱构建方法研究”</span>，BDSY2023004，2023.04-2025.04，在研。
                         </div>
                     </li>
-                
+
                     <li>
-                        <div>烹饪科学四川省高等学校重点实验室2022开放基金项目: <span style="font-weight: bold">“基于多源食品数据的食谱知识图谱构建及其应用”</span> (招标项目), PRKX2022Z01，2023.01-2025.12，在研。
+                        <div>烹饪科学四川省高等学校重点实验室2022开放基金项目: <span style="font-weight: bold">“基于多源食品数据的食谱知识图谱构建及其应用”</span>
+                            (招标项目), PRKX2022Z01，2023.01-2025.12，在研。
                         </div>
                     </li>
                 </ul>
             </div>
 
-            <div>
-                <span>横向项目</span>
+            <div class="project-item">
+                <span class="project-item-name">横向项目</span>
                 <ul>
                     <li>
-                        <div>重庆地理信息与遥感应用中心项目：<span style="font-weight: bold">“缙云山森林灭火资源知识图谱系统构建与应用”</span>，2022.01-2024.01，在研。
+                        <div>重庆地理信息与遥感应用中心项目：<span
+                                style="font-weight: bold">“缙云山森林灭火资源知识图谱系统构建与应用”</span>，2022.01-2024.01，在研。
                         </div>
                     </li>
                 </ul>
             </div>
 
-            <div>
-                <span>科研启动项目</span>
+            <div class="project-item">
+                <span class="project-item-name">科研启动项目</span>
                 <ul>
                     <li>
                         <div>xx项目：<span style="font-weight: bold">“xx”</span>，2023.01-2024.01，在研。
@@ -163,8 +170,8 @@ const projects = [
                 </ul>
             </div>
 
-            <div>
-                <span>教改项目</span>
+            <div class="project-item">
+                <span class="project-item-name">教改项目</span>
             </div>
 
 
@@ -186,49 +193,55 @@ const projects = [
                 <p class="project-text">参与</p>
             </h3>
 
-            <div>
-                <span>省部级（含）以上科研项目</span>
+            <div class="project-item">
+                <span class="project-item-name">省部级（含）以上科研项目</span>
 
-                <ul class="">
-                    <li>
-                        <div>国家自然科学基金委员会，国家自然科学基金面上项目：<span style="font-weight: bold">“面向复杂装备运行状态预测的网络表示学习方法研究”</span>，62376043，2024.01-2027.12，在研。
-                        </div>
-                    </li>
-
-                    <li>
-                        <div>国家自然科学基金委员会，国家自然科学基金面上项目：<span style="font-weight: bold">“知识图谱不确定性计算与推理方法研究”</span>，2024.01-2027.12，在研。 (排名第二)
-                        </div>
-                    </li>
-
-                    <li>
-                        <div>国家自然科学基金委员会，国家自然科学基金面上项目：<span style="font-weight: bold">“基于结构、语义先验知识的属性可控图像生成方法研究”</span>，62171111，2022.01-2024.12，在研。 (排名第二)
-                        </div>
-                    </li>
-                </ul>
-            </div>
-
-
-            <div>
-                <span>横向项目</span>
                 <ul>
                     <li>
-                        <div>华为公司，2022年度CCF-华为胡杨林基金-软件工程专项：<span style="font-weight: bold">“面向研发数据的知识图谱构建技术研究”</span>，2022NSCQ-MSX3749，2022.09-2023.09，在研。
+                        <div>国家自然科学基金委员会，国家自然科学基金面上项目：<span
+                                style="font-weight: bold">“面向复杂装备运行状态预测的网络表示学习方法研究”</span>，62376043，2024.01-2027.12，在研。
+                        </div>
+                    </li>
+
+                    <li>
+                        <div>国家自然科学基金委员会，国家自然科学基金面上项目：<span
+                                style="font-weight: bold">“知识图谱不确定性计算与推理方法研究”</span>，2024.01-2027.12，在研。 (排名第二)
+                        </div>
+                    </li>
+
+                    <li>
+                        <div>国家自然科学基金委员会，国家自然科学基金面上项目：<span
+                                style="font-weight: bold">“基于结构、语义先验知识的属性可控图像生成方法研究”</span>，62171111，2022.01-2024.12，在研。
+                            (排名第二)
                         </div>
                     </li>
                 </ul>
             </div>
-      
-            <div>
-                <span>教改项目</span>
+
+
+            <div class="project-item">
+                <span class="project-item-name">横向项目</span>
+                <ul>
+                    <li>
+                        <div>华为公司，2022年度CCF-华为胡杨林基金-软件工程专项：<span
+                                style="font-weight: bold">“面向研发数据的知识图谱构建技术研究”</span>，2022NSCQ-MSX3749，2022.09-2023.09，在研。
+                        </div>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="project-item">
+                <span class="project-item-name">教改项目</span>
                 <ul class="">
                     <li>
-                        <div>西南大学，2022年校级教育教学改革研究项目：<span style="font-weight: bold">“面向线上教学的高质量个性化教学知识图谱生成方法研究”</span>，2022JY085，2023.01-2024.12，在研。
+                        <div>西南大学，2022年校级教育教学改革研究项目：<span
+                                style="font-weight: bold">“面向线上教学的高质量个性化教学知识图谱生成方法研究”</span>，2022JY085，2023.01-2024.12，在研。
                         </div>
                     </li>
                 </ul>
             </div>
         </div>
-            
+
         <!-- 已结题 -->
         <!--<div>
             <h3>
@@ -236,7 +249,7 @@ const projects = [
             </h3>
         </div>-->
 
-        
+
 
     </div>
 </template>
@@ -266,20 +279,45 @@ const projects = [
     display: flex;
     gap: 10px;
 }
+
 .project-text {
     /* -webkit-text-size-adjust: 100%; */
     list-style: none;
     text-align: left;
-    color: #181818;
+    color: #ff0000;
     word-wrap: break-word;
     white-space: pre-wrap;
     margin: 0;
     padding: 0;
     border: 0;
-    font-weight: normal;
-    font-size: 110%;
-    font-family: "playfair display", sans-serif;
-    line-height: 1.45;
-    font-style: italic;
+    font-weight: 900;
+    font-size: 115%;
+    font-family: Kaiti;
+    /* line-height: 1.45; */
+    /* font-style: italic; */
+}
+
+.project-item {
+    margin: 10px 0px;
+    line-height: 20px;
+    font-family: KaiTi;
+
+}
+
+
+.project-item ul li div {
+    font-family: KaiTi;
+}
+
+.project-item ul li div span {
+    font-family: KaiTi;
+}
+
+.project-item-name {
+    font-size: 16px;
+    font-weight: bold;
+    line-height: 26px;
+    font-family: KaiTi;
+
 }
 </style>
